3baae2989dd0ebffbed9a2cf27f5090d847a8f55
protiks <87400391+protiks@users.noreply.github.com>
2023-03-30 08:27:05 +0200

    temp fix cors

--- server.ts
+++ server.ts
@@ -3,15 +3,19 @@ import bodyParser from 'body-parser'
 import api from './routes/api'
 import swaggerUi from 'swagger-ui-express'
 import YAML from 'yamljs'
+import cors from 'cors'
 
 const app = express()
 const port = process.env.PORT || 3002
 const swaggerDocument = YAML.load('./swagger.yaml')
 
+
+app.use(cors({
+    origin: 'http://localhost:3002'
+}))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use('/api', api)
-
 app.use('/', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 
 console.log('Starting server...')
